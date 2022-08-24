d4f504884d96bd460dcb6cc0f6fc8f3ab26fb444
Jesus-piedrahita96 <jesus-piedrahitakali-jesus.Jesus-piedrahita96>
2022-08-23 20:15:55 -0500

    ajustando el menu mobile con el nav

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ const desktopMenu = document.querySelector('.desktop-menu')
 navEmail.addEventListener('click', () => {
   desktopMenu.classList.toggle('inactive')
 })
+
+const menu = document.querySelector('.menu')
+const menuMobil = document.querySelector('.mobile-menu')
+
+menu.addEventListener('click', () => {
+  menuMobil.classList.toggle('inactive')
+})
+
+const navShopingCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
+
+navShopingCart.addEventListener('click', () => {
+  productDetail.classList.toggle('inactive')
+})
