96001380ae96a2ccd9af1b862882e0a135d827a5
Jesus-piedrahita96 <jesus-piedrahitakali-jesus.Jesus-piedrahita96>
2022-08-23 21:05:23 -0500

    evitando choques entre bloques de opciones

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const navEmail = document.querySelector('.nav-email')
 const desktopMenu = document.querySelector('.desktop-menu')
-
-navEmail.addEventListener('click', () => {
-  desktopMenu.classList.toggle('inactive')
-})
-
 const menu = document.querySelector('.menu')
 const menuMobil = document.querySelector('.mobile-menu')
-
-menu.addEventListener('click', () => {
-  menuMobil.classList.toggle('inactive')
-})
-
 const navShopingCart = document.querySelector('.navbar-shopping-cart')
 const productDetail = document.querySelector('.product-detail')
 
+
+navEmail.addEventListener('click', () => {
+  const productDetailIsClosed = productDetail.classList.contains('inactive')
+
+  if(!productDetailIsClosed){
+    productDetail.classList.add('inactive')
+  }
+  desktopMenu.classList.toggle('inactive')
+})
+
+menu.addEventListener('click', () => {
+  const productDetailIsClosed = productDetail.classList.contains('inactive')
+
+  if(!productDetailIsClosed){
+    productDetail.classList.add('inactive')
+  }
+
+  menuMobil.classList.toggle('inactive')
+})
+
 navShopingCart.addEventListener('click', () => {
+  const menuMobilIsClosed = menuMobil.classList.contains('inactive')
+  const desktopMenuIsClosed = desktopMenu.classList.contains('inactive')
+
+  if(!menuMobilIsClosed){
+    menuMobil.classList.add('inactive')
+  }
+
+  if(!desktopMenuIsClosed){
+    desktopMenu.classList.add('inactive')
+  }
+
   productDetail.classList.toggle('inactive')
 })
+
