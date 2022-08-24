65822f25d0fd7e96809f4418912356b3f3382f9b
Jesus-piedrahita96 <jesus-piedrahitakali-jesus.Jesus-piedrahita96>
2022-08-24 17:44:46 -0500

    agregando dinamismo en la repeticion de cards en html

--- index.js
+++ index.js
@@ -40,3 +40,60 @@ navShopingCart.addEventListener('click', () => {
   productDetail.classList.toggle('inactive')
 })
 
+const productList = []
+productList.push({
+  name : 'Bike',
+  price : 120.00,
+  image : 'https://olimpica.vtexassets.com/arquivos/ids/524355/Bicicleta-RALI-RIO-Mec%EF%BF%BDnica-29-Hombre--Negro-Verde.jpg?v=637558583193570000'
+},
+{
+  name : 'Mouse',
+  price : 25.00,
+  image: 'https://http2.mlstatic.com/D_NQ_NP_739866-MLA47920791587_102021-O.jpg'
+},
+{
+  name : 'monitor',
+  price : 200.00,
+  image: 'https://m.media-amazon.com/images/I/71mU5rpECpL._AC_SL1385_.jpg'
+})
+
+const productCardContainer = document.querySelector('.cards-container')
+
+function renderCard(list){
+  for (product of list){
+    const productCard = document.createElement('div')
+    productCard.classList.add('product-card')
+
+    const productImage = document.createElement('img')
+    productImage.setAttribute('src', product.image)
+
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info')
+
+    const productDiv = document.createElement('div')
+    const productPrice = document.createElement('p')
+    productPrice.innerText = '$'+product.price
+    const productName = document.createElement('p')
+    productName.innerText = product.name
+
+    const productFigure = document.createElement('figure')
+    const productImg = document.createElement('img')
+    productImg.setAttribute('src', './Icons/bt_add_to_cart.svg')
+
+    productFigure.appendChild(productImg)
+
+    productDiv.appendChild(productPrice)
+    productDiv.appendChild(productName)
+
+    productInfo.appendChild(productDiv)
+    productInfo.appendChild(productFigure)
+
+    productCard.appendChild(productImage)
+    productCard.appendChild(productInfo)
+
+    productCardContainer.appendChild(productCard)
+
+  }
+}
+
+renderCard(productList)
